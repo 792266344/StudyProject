30b6e714b3c2bd3925bead37d810d933e9663abc
DG <792266344@qq.com>
2020-01-18 16:23:20 +0800

    add traversal

--- studyproject/bin/main.dart
+++ studyproject/bin/main.dart
@@ -5,11 +5,11 @@ import '../lib/linkedlist.dart';
 
 void main() {
   BinaryTree tree=BinaryTree<int>();
-  print(tree.size);
+  // print(tree.size);
   tree.add(5);
   tree.add(6);
   tree.add(1);
   tree.add(3);
   tree.add(8);
-  print(tree.size);
+  tree.postorder();
 }
--- studyproject/lib/binarytree.dart
+++ studyproject/lib/binarytree.dart
@@ -27,6 +27,30 @@ class BinaryTree<T> {
     steps = _steps + 1;
     size++;
   }
+
+  void preorder()=>_preorder(_root);
+
+  void _preorder(BinaryTreeNode node){
+    if(node.leftchild!=null)_preorder(node.leftchild);
+    if(node.rightchild!=null)_preorder(node.rightchild);
+    print(node.data);
+  }
+
+  void inorder()=>_inorder(_root);
+
+  void _inorder(BinaryTreeNode node){
+    if(node.leftchild!=null)_inorder(node.leftchild);
+    print(node.data);
+    if(node.rightchild!=null)_inorder(node.rightchild);
+  }
+
+  void postorder()=>_postorder(_root);
+
+  void _postorder(BinaryTreeNode node){
+    print(node.data);
+    if(node.leftchild!=null)_postorder(node.leftchild);
+    if(node.rightchild!=null)_postorder(node.rightchild);
+  }
 }
 
 class BinaryTreeNode<T> {
